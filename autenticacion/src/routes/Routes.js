0d6cd21887734001c43032b63c481fb89bc96daf
Jose Adrián <josealvarado@unicomfacauca.edu.co>
2022-04-11 18:55:12 -0500

    segundo commit

---
+++ autenticacion/src/routes/Routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from '../pages/Login';
+import Menu from '../pages/Menu';
+
+
+function Routes() {
+  return (
+    /*<BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Login}/>
+        <Route exact path="menu" component={Menu}/>
+      </Switch>
+    </BrowserRouter>*/
+    <div>
+      <Login/>
+      <Menu/>
+    </div>
+
+  );
+}
+
+export default Routes;
